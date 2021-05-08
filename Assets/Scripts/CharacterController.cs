5e7ceecaf1ab2c43e86b0535d5419703f4e7af0e
Tobias Kloss <me@tobiaskloss.com>
2021-05-08 14:40:02 +0200

    commit

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,33 +1,56 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Scriptableobjects;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField]
-    private float jumpForce = 10;
-    
-    
-    [SerializeField]
-    private InputAction _jumpAction;
+    [SerializeField] private CharacterSettings _characterSettings;
+
+    [SerializeField] private InputAction _jumpAction;
 
     private Rigidbody2D _rigidbody2D;
-    
-    
+
+    [Header("Ground Check")] [SerializeField]
+    private bool _checkForGrounding;
+
+    [SerializeField] private LayerMask _groundLayers;
+    [SerializeField] private Transform _groundCheckPosition;
+    private bool _grounded;
+
+
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
-   
+
     void Start()
     {
         _jumpAction.performed += context =>
         {
-            _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            if (_grounded)
+            {
+                _rigidbody2D.AddForce(Vector2.up * _characterSettings.jumpForce, ForceMode2D.Impulse);
+            }
         };
+    }
+
+    private void FixedUpdate()
+    {
+        _grounded = false;
+
+        var colliders = Physics2D.OverlapCircleAll(_groundCheckPosition.position, 0.2f, _groundLayers);
+
+        foreach (var collider in colliders)
+        {
+            if (collider.gameObject != gameObject)
+            {
+                _grounded = true;
+            }
+        }
     }
 
     private void OnEnable()
---
+++ Assets/Scripts/Scriptableobjects/CharacterSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Scriptableobjects
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Character Settings", order = 1)]
+
+    public class CharacterSettings: ScriptableObject
+    {
+        public float jumpForce;
+        public float walkSpeed;
+    }
+}
